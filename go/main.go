1b3dd645ec80ffa628e54eff85748b504ae278bd
Bowen <WenTesla@163.com>
2023-01-19 12:14:03 +0800

    项目初始化

--- go/main.go
+++ go/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"tiktok/go/route"
 )
 
+/*
+启动类
+*/
 func main() {
 	// 1.创建路由
 	r := gin.Default()
@@ -13,5 +17,8 @@ func main() {
 	route.LoadRouter(r)
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
-	r.Run(":8000")
+	err := r.Run(":8000")
+	if err != nil {
+		fmt.Print("项目启动失败")
+	}
 }
